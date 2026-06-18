c8737e517e8eb4237a88cf07866bd8667c364227
Eduardo dos Santos <perfilprofissional.eduardo@gmail.com>
2026-06-18 10:58:32 -0300

    Construção do Grafo Versão 1.0

--- include/fsm.h
+++ include/fsm.h
@@ -1,1 +1,22 @@
 //o comportamento propriamente dito usando o FSM
+//o comportamento propriamente dito usando o FSM
+#ifndef FSM_H
+#define FSM_H
+#define MAXNODES 3//definição dos nós para usar o gráfo direcionado, a partir da matriz de adjacência
+
+typedef struct {
+		int vertice_number;
+		int matriz[MAXNODES][MAXNODES];
+}adjacency_matrix;
+
+void initialize_graph(adjacency_matrix *graph, int number_vertice){
+    graph->vertice_number = number_vertice;
+
+    for (int i = 0; i < number_vertice; i++){
+        for (int j = 0; j < number_vertice; j++){
+            graph->matriz[i][j] = 0;
+        }
+    }
+}
+
+#endif            
