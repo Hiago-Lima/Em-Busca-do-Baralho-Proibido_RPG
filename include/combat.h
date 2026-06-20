3eb860c2d7839c2416949bb7d925714b05eb9bca
Hiago Lima <hiago.olima1@gmail.com>
2026-06-20 03:01:37 -0300

    so arrumei comentarios

--- include/combat.h
+++ include/combat.h
@@ -7,17 +7,16 @@
 #include "stack.h"
 
 typedef enum {
-    COMBAT_VICTORY,  /* Jogador venceu */
-    COMBAT_DEFEAT,   /* Jogador perdeu */
-    COMBAT_FLED      /* Jogador fugiu  */
+    COMBAT_VICTORY,  // Jogador venceu 
+    COMBAT_DEFEAT,   // Jogador perdeu 
+    COMBAT_FLED      // Jogador fugiu  
 } CombatResult;
 
-/*
- * Executa um combate completo.
- * Acumula o histórico de ações na `history` passada.
- * Retorna o resultado.
- */
+
+//Executa um combate completo.
+ //Acumula o histórico de ações na `history` passada.
+ // Retorna o resultado.
 CombatResult combat_run(Character *player, Enemy *enemy,
                         ActionStack *history);
 
-#endif /* COMBAT_H */
+#endif // COMBAT_H 
--- include/enemies.h
+++ include/enemies.h
@@ -5,7 +5,7 @@
 #include "character.h"
 #include "fsm.h"
 
-/* Um inimigo é um personagem + sua FSM */
+// Um inimigo é um personagem + sua FSM 
 typedef struct {
     Character character;
     FSM       fsm; 
@@ -19,7 +19,7 @@ void enemy_create_dark_mage(Enemy *e);
 
 // Cria o Dragão Branco — boss com 3 fases baseadas em HP 
 void enemy_create_blue_eyes(Enemy *e);
-
+    
 // Consulta a FSM e retorna o estado decidido para este turno
 FSMState enemy_think(Enemy *e, const Character *player);
 
